d98a07ad57f9249ef90206a79cc76929683ec49a
sjory3 <Mgj32000@gmail.com>
2022-04-28 10:20:16 +0200

    dependens
    added more libaries for using google sheets, also added a tabel in the desktop

--- ts/src/desktop/desktop.ts
+++ ts/src/desktop/desktop.ts
@@ -1,6 +1,8 @@
 import { AppWindow } from "../AppWindow";
 import { kWindowNames } from "../consts";
 
+
+
 // The desktop window is the window displayed while game is not running.
 // In our case, our desktop window has no logic - it only displays static data.
 // Therefore, only the generic AppWindow class is called.
