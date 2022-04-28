6249c81b88282460d717e1ae7ae52aa38f27b718
sjory3 <Mgj32000@gmail.com>
2022-04-28 15:04:19 +0200

    testing
    made a web app on the google app sripts to get acces to the google sheets
    
    it works, but i cant read the responce as json i get a error

--- ts/src/desktop/desktop.ts
+++ ts/src/desktop/desktop.ts
@@ -1,6 +1,22 @@
+import { GoogleAuth } from "google-auth-library";
 import { AppWindow } from "../AppWindow";
 import { kWindowNames } from "../consts";
 
+
+function getData(){   
+    const url = "https://script.google.com/macros/s/AKfycbz4PwWwNyg5SDuizRr-X-IzqJ2LAaWrSqM4u63cy25LJB2MEjiLqG9efmjccsrb9xaE/exec";
+    fetch(url,{mode: 'no-cors',})
+    .then(d => d.json())
+    .then(d => {
+        document.getElementById("middle").textContent =  d[1];
+        console.log(d[0]);
+        console.log(d[1]);
+        console.log(d[2]);
+        console.log(d[3]);
+    });
+}
+
+document.getElementById("btn").addEventListener("click", getData); 
 
 
 // The desktop window is the window displayed while game is not running.
