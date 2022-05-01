68ac5a9c2dd03162a1b5870add0f96f8c40f8cf6
Sjory <Mgj32000@gmail.com>
2022-05-02 00:07:47 +0200

    idfk
    yes

--- ts/src/desktop/desktop.ts
+++ ts/src/desktop/desktop.ts
@@ -1,23 +1,24 @@
-import { GoogleAuth } from "google-auth-library";
 import { AppWindow } from "../AppWindow";
 import { kWindowNames } from "../consts";
 
 
+let data;
+
 function getData(){   
-    const url = "https://script.google.com/macros/s/AKfycbz4PwWwNyg5SDuizRr-X-IzqJ2LAaWrSqM4u63cy25LJB2MEjiLqG9efmjccsrb9xaE/exec";
-    fetch(url,{mode: 'no-cors',})
+    const url = "https://script.google.com/macros/s/AKfycbwD8pCJpdyJrR4mEFvcrdfYsdLFgqBddOipMXPTHWC0iqXP3J3LHg5CFA4GrpVUP9gX/exec";
+    fetch(url,{mode: 'no-cors', method: 'GET'})
     .then(d => d.json())
-    .then(d => {
-        document.getElementById("middle").textContent =  d[1];
-        console.log(d[0]);
-        console.log(d[1]);
-        console.log(d[2]);
-        console.log(d[3]);
+    .then((json) => {
+        document.getElementById("middle").textContent =  json[1];
+        console.log(json[0]);
+        console.log(json[1]);
+        console.log(json[2]);
+        console.log(json[3]);
     });
+
 }
 
 document.getElementById("btn").addEventListener("click", getData); 
-
 
 // The desktop window is the window displayed while game is not running.
 // In our case, our desktop window has no logic - it only displays static data.
